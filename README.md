e8580a8ef8d8bddc1af7ab4da36335cfadbeb18f
kofman10 <kajotonis@gmal.com>
2023-09-11 19:14:42 +0100

    final

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# Sample Hardhat Project
+# Overview
+NFTtoken = 0xFA6BBAfA2A5a68Fa1702e24a194ebf7edfde9b96
+contract = https://sepolia.etherscan.io/address/0xFA6BBAfA2A5a68Fa1702e24a194ebf7edfde9b96#code
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
--- contracts/KofoNFT.sol
+++ contracts/KofoNFT.sol
@@ -1,6 +1,6 @@
 // Contract based on https://docs.openzeppelin.com/contracts/4.x/erc721
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -12,7 +12,7 @@ string public tokenURI = 'ipfs://QmUzrJMMgtn7isRLv8GXZ4gBHLjVyhrVkd7KvYwYYHiKvh'
 
    constructor() ERC721("KofoNFT", "KFT") {
       _mint(msg.sender, 1);
-       _setTokenURI(0, tokenURI);
+       _setTokenURI(1, tokenURI);
    }
 
 }
