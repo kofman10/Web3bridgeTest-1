9c04699d4c7a20545fda982cf62d9313f7bc56ad
kofman10 <kajotonis@gmal.com>
2023-09-13 19:16:41 +0100

    push

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -6,24 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LMAOToken is ERC20, Ownable {
     address public feeAddress;
-    uint256 public feePercentage = 8; // 8% fee
+    uint256 public feePercentage = 8; 
 
     constructor(address _feeAddress) ERC20("LMAO Token", "LMAO") {
         feeAddress = _feeAddress;
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
-    // Function to set the fee address
-    function setFeeAddress(address _newFeeAddress) external onlyOwner {
-        feeAddress = _newFeeAddress;
-    }
 
-    // Function to set the fee percentage
-    function setFeePercentage(uint256 _newFeePercentage) external onlyOwner {
-        require(_newFeePercentage <= 100, "Fee percentage cannot exceed 100%");
-        feePercentage = _newFeePercentage;
-    }
-
-    // Transfer function with a fee
     function transfer(address recipient, uint256 amount) public override returns (bool) {
         uint256 feeAmount = (amount * feePercentage) / 100;
         uint256 transferAmount = amount - feeAmount;
